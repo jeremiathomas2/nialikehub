a5a076df7b6da2a009660fdb45b205a7a176875f
Jeremia Thomas <142270376+jeremiathomas2@users.noreply.github.com>
2026-08-23 18:10:55 +0300

    Apply system favicon package across all templates

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("", RedirectView.as_view(url="/dashboard/", permanent=False)),
+    path("favicon.ico", RedirectView.as_view(url="/static/img/favicon/favicon.ico", permanent=True)),
     path("", include("apps.accounts.urls")),
     path("", include("apps.core.urls")),
     path("", include("apps.events.urls")),
